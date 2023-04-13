01b16e948abbb22415c718727dcf40e0cd39c439
mahardikayurico <mahardikayurico.my@gmail.com>
2023-04-13 21:51:38 +0700

    fix: add CRUD candidates

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CandidateController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -27,5 +28,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::resource('candidates', CandidateController::class);
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
