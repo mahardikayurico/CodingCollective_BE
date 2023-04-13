8df1a1b2fa59de40c38d9ca0b00011f8303de89a
mahardikayurico <mahardikayurico.my@gmail.com>
2023-04-14 01:01:24 +0700

    fix: update laravel-passport API

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth:api');
